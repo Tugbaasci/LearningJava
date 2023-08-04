ea933e3e60dd683098cb8c3c9986872676522ff1
Tuba Asci <tugba.asci1419@gmail.com>
2023-08-05 01:50:37 +0200

    alfabeyi loopla yazdirma

---
+++ src/sample_questions/soru25.java
@@ -0,0 +1,15 @@
+package sample_questions;
+
+public class soru25 {
+    public static void main(String[] args) {
+        /*
+        Alfabeyi konsola yazdırın.
+        örnek:
+        a b c .. .. .. .. y z
+        ipucu: char kullanmak iyi bir fikir olabilir
+         */
+        for (char i = 'a'; i <='z' ; i++) {
+            System.out.print(i+" ");
+        }
+    }
+}
