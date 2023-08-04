123ec4dc14323c530e5f5c7c3b7dc03f07704a04
Tuba Asci <tugba.asci1419@gmail.com>
2023-08-05 01:32:45 +0200

    ebob-ekok ve faktoryel-bulma sorusu

---
+++ src/sample_questions/soru23.java
@@ -0,0 +1,31 @@
+package sample_questions;
+
+import java.util.Scanner;
+
+public class soru23 {
+    public static void main(String[] args) {
+        /*
+        Girdi olarak bir tamsayı kabul eden ve
+        bu tam sayının faktöriyelini hesaplayan bir method yazın.
+        Method bize çıktıyı döndürsün.
+        Örnek:
+        Girdi: 6
+        Çıktı: 6!=65432*1=720
+        ipucu: Aldıgınız sayıyı methoda gönderin.
+        Methodun içinde For Loop ile faktöriyelini hesaplayın
+         */
+        Scanner scanner= new Scanner(System.in);
+        System.out.print("Lütfen faktoryeli alinacak olan sayiyi giriniz:");
+        int sayi = scanner.nextInt();
+        faktoryel(sayi);
+    }
+    public static void faktoryel(int sayi){
+        int carpim=1;
+        String crp ="";
+        for (int i = sayi; i >=2 ; i--) {
+            crp+=i+"x";
+            carpim*=i;
+        }
+        System.out.println(sayi+"!="+crp+"1="+carpim);
+    }
+}
