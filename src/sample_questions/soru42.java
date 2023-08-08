407a9d2ae42c23b1bf78b654db111071ec039208
Tuba Asci <tugba.asci1419@gmail.com>
2023-08-08 15:43:14 +0200

    basit cons olusturma-cagirma

--- src/sample_questions/soru42.java
+++ src/sample_questions/soru42.java
@@ -1,13 +1,6 @@
 package sample_questions;
 
-public class soru42coz {
-    soru42coz(){
-      String renk = "kirmizi";
-    }
-    soru42coz(int sayi){
-      sayi=2*sayi;
-    }
-    public static void main(String[] args) {
+public class soru42{
         /*
         Biri parametreli, diğeri parametresiz 2 constructor oluşturun.
         Bu const. kullanarak en az 2 nesne oluşturun
@@ -16,8 +9,30 @@ public class soru42coz {
         Nesnelerin rengini adını sayısını gönderelim
         sonra cagırıp konsolda yazdıralım
          */
-        soru42coz obj = new soru42coz();
-        soru42coz obj2 = new soru42coz(5);
-        System.out.println(re);
+        String isim="Deger Atanmamis";
+        String soyad="Deger Atanmamis";
+         static String renk ="deger atanmadi";
+        int yas=0;
+    public static void main(String[] args) {
+        soru42 obj1=new soru42();
+        soru42 obj2=new soru42("ali","yılmaz");
+        soru42 obj3 = new soru42("tugba","asci",39);
+        System.out.println(new soru42("yali", "sami"));
+        System.out.println(renk);
     }
-}
+    soru42(){
+        System.out.println("Parametresiz cons calisti");
+    }
+    soru42(String isim,String soyad){
+        this.soyad=soyad;
+        this.isim=isim;
+        System.out.println(isim+" "+soyad);
+    }
+    soru42(String isim, String soyad,int yas){
+        this.soyad=soyad;
+        this.isim=isim;
+        this.yas= yas;
+        System.out.println(isim+" "+soyad+" yas: "+yas);
+    }
+    }
+
