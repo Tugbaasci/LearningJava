888aef222d9ad23d7f6e41b117120ee2c4f5ff87
Tuba Asci <tugba.asci1419@gmail.com>
2023-08-14 23:42:36 +0200

    kullanicidan alarak soru cozumu

--- src/soru1.java
+++ src/soru1.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class soru1 {
     public static void main(String[] args) {
  /*1---
@@ -8,11 +10,21 @@ public class soru1 {
     Beklenen Cikti
     7
     */
-        int sayi=34;
-        int birler=sayi%10;
-        sayi/=10;
-        int onlar=sayi%10;
-        int toplam= birler+onlar;
-        System.out.println("Beklenen Cikti: "+toplam);
+        //int sayi=34;
+        //int birler=sayi%10;
+        //sayi/=10;
+        //int onlar=sayi%10;
+        //int toplam= birler+onlar;
+        //System.out.println("Beklenen Cikti: "+toplam);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayi girin:");
+        int sayi= scanner.nextInt();
+        int adet=0, toplam=0;
+        while(sayi>0){//sayi!=0 olunca da calisiyor
+            toplam=(sayi%10)+toplam;
+            sayi/=10;
+            ++adet;
+        }
+        System.out.println("basamak toplami: "+toplam);
     }
 }
