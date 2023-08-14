7d3dc1c901ba30c43e9120b44877f9d36ac9160d
Tuba Asci <tugba.asci1419@gmail.com>
2023-08-14 14:59:20 +0200

    array list son indexten baslayarak elemanlari yazdirma

---
+++ src/javaPractice/isimleriListedenSonIndextenYaz.java
@@ -0,0 +1,33 @@
+package javaPractice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class isimleriListedenSonIndextenYaz {
+    public static void main(String[] args) {
+        // Bir ArrayList oluşturun ve içine 5 adet isim ekleyin.
+        // Daha sonra bu isimleri sondan başlayarak ekrana yazdırın.
+        // Örnek çıktı:
+        // Son isim: Ahmet
+        // Son ikinci isim: Mehmet
+        // ...
+        List<String> isim = new ArrayList<>();
+        isim.add("esra");
+        isim.add("kubra");
+        isim.add("ceyda");
+        isim.add("filiz");
+        isim.add("azra");
+        List<String> yeniList= new ArrayList<>();
+        for (int i =isim.size()-1; i >=0 ; i--) {
+            yeniList.add(isim.get(i));
+            System.out.println((yeniList.indexOf(isim.get(i))+1)+".isim:"+isim.get(i));
+        }
+
+
+
+
+
+
+
+    }
+}
